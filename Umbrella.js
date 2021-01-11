49c63bd07d2e889f015621993b9f26f7175f77d5
Ryan-ops68 <72042413+Ryan-ops68@users.noreply.github.com>
2021-01-10 19:37:32 -0500

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,25 @@
+class Umbrella {
+    constructor()}
+
+    var batman;
+    batmanImage = loadImage("/batman");
+    batmanImage = addImage("/batman");
+
+    ellipse();
+    
+    update();
+
+    display();
+
+    setup();
+    
+    var maxDrops=100;
+for(var i=0, i<maxDrops; i++){
+    drops.push(new createDrop(random(0,400), random(0, 400)));
+}
+{
+
+}
+
+CreateCanvas();
+
